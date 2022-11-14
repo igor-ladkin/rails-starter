72165c81b663941069f391bcaf10eece74e14c43
Igor Ladkin <dgkplan@googlemail.com>
2022-11-14 21:28:03 +0100

    Pass current user into graphql resolution context

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -11,7 +11,7 @@ module Types
     field :test_field, String, null: false,
       description: "An example field added by the generator"
     def test_field
-      "Hello World!"
+      "Hello #{context[:current_user]&.name&.capitalize || 'World'}!"
     end
   end
 end
