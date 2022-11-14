ed66b78d8e43a1156aac87d4f4d6f1aedc1aa540
Igor Ladkin <dgkplan@googlemail.com>
2022-11-14 20:52:24 +0100

    Add controller helpers to mimic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  helper_method :set_current_user, :current_user, :signed_in?, :user_param, :user_header
+
+  before_action :set_current_user
+
+  def set_current_user
+    @current_user = Accounts::User.find_by name: (user_param || user_header)
+  end
+  attr_reader :current_user
+
+  def signed_in?
+    current_user.present?
+  end
+
+  def user_param
+    params[:u].presence
+  end
+
+  def user_header
+    request.headers["X-User"].presence
+  end
 end
